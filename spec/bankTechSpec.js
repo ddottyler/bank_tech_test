d3cec505acf674e651fb8b663064ff772271a8c8
Dan T <dp.tyler@hotmail.co.uk>
2021-03-08 20:34:34 +0000

    Statement outputting date, credit, debit and balance with tests passing

--- spec/bankTechSpec.js
+++ spec/bankTechSpec.js
@@ -13,35 +13,50 @@ describe("bankTech", function(){
 
 // Tests are below
 
-    describe('deposit', function() {
-      it('should deposit x amount to balance', function(){
-        bank.deposit(500);
-        expect(bank._balance).toBe(500);
-      })
-
-      it('should make a full record of deposit, including date, transaction type and total balance', function(){
-        bank.deposit(500);
-        expect(bank._transactionHistory[0]).toEqual(
-          {date: "8/3/2021", credit: 500.00, debit: null, balance: 500.00}
-          )
-      })
-
+  describe('deposit', function() {
+    it('should deposit x amount to balance', function(){
+      bank.deposit(500);
+      expect(bank._balance).toBe(500);
     })
 
-    describe('withdraw', function(){
-      it('should withdraw x amount from balance', function(){
-        depositAndWithdraw()
-        expect(bank._balance).toBe(0);
-      })
-
-      it('should make a full record of withdrawal, including date, transaction type and total balance', function(){
-        depositAndWithdraw()
-        expect(bank._transactionHistory[1]).toEqual(
-          {date: "8/3/2021", credit: null, debit: 500.00, balance: 0}
+    it('should make a full record of deposit, including date, transaction type and total balance', function(){
+      bank.deposit(500);
+      expect(bank._transactionHistory[0]).toEqual(
+        {date: "8/3/2021", credit: 500.00, debit:'', balance: 500.00}
         )
-      })
-
     })
+
+  })
+
+  describe('withdraw', function(){
+    it('should withdraw x amount from balance', function(){
+      depositAndWithdraw()
+      expect(bank._balance).toBe(0);
+    })
+
+    it('should make a full record of withdrawal, including date, transaction type and total balance', function(){
+      depositAndWithdraw()
+      expect(bank._transactionHistory[1]).toEqual(
+        {date: "8/3/2021", credit:'', debit: 500.00, balance: 0}
+      )
+    })
+
+  })
+
+  describe('showStatement', function(){
+    it('should show full details of all transactions', function(){
+      bank.deposit(1000);
+      bank.deposit(2000);
+      bank.withdraw(500);
+      expect(bank.showStatement()).toEqual(
+        'date || credit || debit || balance\n' +
+        '8/3/2021 ||  || 500 || 2500\n' +
+        '8/3/2021 || 2000 ||  || 3000\n' +
+        '8/3/2021 || 1000 ||  || 1000\n'
+      )
+    })
+
+  })
   
 })
 
--- src/bankTech.js
+++ src/bankTech.js
@@ -7,14 +7,26 @@ class bankTech {
 
   deposit(amount) {
     this._balance += amount
-    let depositDetails = {date: this.date(), credit: amount, debit: null, balance: this._balance}
+    let depositDetails = {date: this.date(), credit: amount, debit:'', balance: this._balance}
     this._transactionHistory.push(depositDetails)
+    // localStorage.setItem("transactionHistory", this._transactionHistory);
   }
 
   withdraw(amount) {
     this._balance -= amount
-    let withdrawalDetails = {date: this.date(), credit: null, debit: amount, balance: this._balance}
+    let withdrawalDetails = {date: this.date(), credit:'', debit: amount, balance: this._balance}
     this._transactionHistory.push(withdrawalDetails)
+    // localStorage.setItem("transactionHistory", this._transactionHistory);
+  }
+
+  showStatement() {
+    let tableTop = 'date || credit || debit || balance\n'
+    let update;
+    this._transactionHistory.reverse().forEach(transaction => update = tableTop += transaction.date +' || '+ transaction.credit +' || '+ transaction.debit +' || '+ transaction.balance +'\n')
+    return update
+    // console.log('date || credit || debit || balance')
+    // for(let i = this._transactionHistory.length - 1; i >= 0; i--)
+    // console.log(`${this._transactionHistory[i].date} || ${this._transactionHistory[i].credit} || ${this._transactionHistory[i].debit} || ${this._transactionHistory[i].balance}`)
   }
 
   date() {
