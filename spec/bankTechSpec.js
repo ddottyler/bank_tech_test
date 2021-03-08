3aede9aa5525a32c33a6ee1defd7bde307ddf3ed
Dan T <dp.tyler@hotmail.co.uk>
2021-03-08 15:43:15 +0000

    Deposit method updated to include details including date, transaction type and balance

--- spec/bankTechSpec.js
+++ spec/bankTechSpec.js
@@ -5,12 +5,21 @@ describe("bankTech", function(){
     bank = new bankTech();
   })
 
-    describe('balance', function() {
+    describe('deposit', function() {
       it('should deposit x amount to balance', function(){
         bank.deposit(500);
         expect(bank._balance).toBe(500);
       })
 
+      it('should make a full record of deposit, including date, transaction type and total balance', function(){
+        bank.deposit(500);
+        expect(bank._transactionHistory[0]).toEqual(
+          {date: "8/3/2021", credit: 500.00, debit: null, balance: 500.00})
+      })
+
+    })
+
+    describe('withdraw', function(){
       it('should withdraw x amount from balance', function(){
         bank.deposit(500);
         bank.withdraw(500);
--- src/bankTech.js
+++ src/bankTech.js
@@ -2,14 +2,26 @@ class bankTech {
 
   constructor() {
     this._balance = 0;
+    this._transactionHistory = []
   }
 
   deposit(amount) {
     this._balance += amount
+    let depositDetails = {date: this.date(), credit: amount, debit: null, balance: this._balance}
+    this._transactionHistory.push(depositDetails)
   }
 
   withdraw(amount) {
     this._balance -= amount
+  }
+
+  date() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1;
+    let yyyy = today.getFullYear();
+    let dateInfo = `${dd}/${mm}/${yyyy}`;
+    return dateInfo
   }
 
 }
